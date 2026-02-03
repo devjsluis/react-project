2cde8d3db820fab23a59e6672f5fe0086df8ae42
Luis Ángel <devjsluis@gmail.com>
2026-02-03 17:57:07 -0600

    Hola mundo

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ function App() {
     <div
       style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}
     >
-      <h1 style={{ color: "#61dafb" }}>¡Hola! Soy Ángel</h1>
-      <div
-        style={{
-          padding: "20px",
-          border: "1px solid #444",
-          borderRadius: "10px",
-          display: "inline-block",
-        }}
-      >
-        <h3>Estado del Servidor AWS:</h3>
-        <p style={{ color: "#ff9900" }}>
-          ⏳ Waiting aprobación de cuota (24h)...
-        </p>
-      </div>
+      <h1>Hola mundo</h1>
     </div>
   );
 }
